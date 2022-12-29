7eacb62768221fd7184d80ed2329a5647dbe61c7
mford1999 <malo.ford@witty-services.fr>
2022-12-29 12:13:19 +0100

    ng add command

--- src/paddls-schematics/complete-install.ts
+++ src/paddls-schematics/complete-install.ts
@@ -1,13 +1,13 @@
 import {chain, Rule, schematic, SchematicContext, Tree} from '@angular-devkit/schematics';
 
-export function completeInstall(options: any): Rule {
+export function ngAdd(options: any): Rule {
     return (tree: Tree, context: SchematicContext) => {
         return chain([
-            schematic('ng-add', options),
-            schematic('ng-modules', options),
-            schematic('ng-modules-files', options),
-            schematic('ng-replace-environment', options),
-            schematic('ng-replace-app-module-file', options)
+            schematic('add-paddls', options),
+            schematic('add-modules', options),
+            schematic('add-modules-files', options),
+            schematic('replace-environment', options),
+            schematic('replace-app-module-file', options)
         ])(tree, context);
     };
 }
