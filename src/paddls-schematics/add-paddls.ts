17271bb9e1d918a8c84dceb4a7f027c08eb0b1da
mford1999 <malo.ford@witty-services.fr>
2023-01-02 14:59:23 +0100

    fix message versions always displayed

--- src/paddls-schematics/add-paddls.ts
+++ src/paddls-schematics/add-paddls.ts
@@ -1,4 +1,4 @@
-import {Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
+import {chain, Rule, schematic, SchematicContext, Tree} from '@angular-devkit/schematics';
 import {NodePackageInstallTask} from '@angular-devkit/schematics/tasks';
 import {execSync} from 'child_process';
 import {
@@ -14,80 +14,216 @@ import {
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function addDependencies(options: any): Rule {
-    return (tree: Tree, context: SchematicContext) => {
+    return (_tree: Tree, context: SchematicContext) => {
 
         const dep: string[] = options.choiceLibraries;
-        const angularVersion: number | undefined = getAngularVersion(tree);
-        const rxjsVersion: number | undefined = getRxjsVersion(tree);
-        let dependency: NodeDependency | undefined;
+        const ruleNgxRepo: Rule = schematic('add-ngx', options);
+        const ruleNgxHttp: Rule = schematic('add-ngx-http', options);
+        const ruleNgxFirestore: Rule = schematic('add-ngx-firestore', options);
+        const ruleNgxCommon: Rule = schematic('add-ngx-common', options);
+        const ruleNgxSerializer: Rule = schematic('add-ngx-serializer', options);
+        const ruleRxjsCommon: Rule = schematic('add-rxjs-common', options);
+        const ruleTsSerializer: Rule = schematic('add-ts-serializer', options);
+        let rules: Rule[] = [];
 
         context.logger.info(`\n Installing the @paddls dependencies selected`)
-        dep.forEach((dependancy: string) => {
-            context.logger.info(`${dependancy}`);
 
-            const command: string = `npm show ${dependancy} versions`;
-            let output: string = execSync(command, {
-                encoding: 'utf8',
-            });
+        dep.forEach((dependency: string) => {
+            context.logger.info(`${dependency}`);
 
-            output = output.replace(/'/g, '"');
-            output = JSON.parse(output);
+                if (dependency === '@paddls/ngx-repository') {
+                    rules.push(ruleNgxRepo);
+                } else if (dependency === '@paddls/ngx-http-repository') {
+                    rules.push(ruleNgxHttp);
+                } else if (dependency === '@paddls/ngx-firestore-repository') {
+                    rules.push(ruleNgxFirestore);
+                }  else if (dependency === '@paddls/ngx-common') {
+                    rules.push(ruleNgxCommon);
+                } else if (dependency === '@paddls/ngx-serializer') {
+                    rules.push(ruleNgxSerializer);
+                } else if (dependency === '@paddls/rxjs-common') {
+                    rules.push(ruleRxjsCommon);
+                } else if (dependency === '@paddls/ts-serializer') {
+                    rules.push(ruleTsSerializer);
+                }
 
-            if (options.ngxRepositoryVersion && (dependancy === '@paddls/ngx-repository' || dependancy === '@paddls/ngx-http-repository' || dependancy === '@paddls/ngx-firestore-repository')) {
-                dependency = createNodeDependence(dependancy, options.ngxRepositoryVersion, NodeDependencyType.Default);
-            } else if (options.ngxCommonVersion && dependancy === '@paddls/ngx-common') {
-                dependency = createNodeDependence(dependancy, options.ngxCommonVersion, NodeDependencyType.Default);
-            } else if (options.ngxSerializerVersion && dependancy === '@paddls/ngx-serializer') {
-                dependency = createNodeDependence(dependancy, options.ngxSerializerVersion, NodeDependencyType.Default);
-            } else if (options.rxjsCommonVersion && dependancy === '@paddls/rxjs-common') {
-                dependency = createNodeDependence(dependancy, options.rxjsCommonVersion, NodeDependencyType.Default);
-            } else if (options.tsSerializerVersion && dependancy === '@paddls/ts-serializer') {
-                dependency = createNodeDependence(dependancy, options.tsSerializerVersion, NodeDependencyType.Default);
-            } else if (angularVersion === 14 && dependancy !== '@paddls/rxjs-common' && dependancy !== '@paddls/ts-serializer') {
-                if (dependancy === '@paddls/ngx-repository' || dependancy === '@paddls/ngx-http-repository' || dependancy === '@paddls/ngx-firestore-repository') {
-                    dependency = createNodeDependence(dependancy, '4.0.1', NodeDependencyType.Default);
-                } else if (dependancy === '@paddls/ngx-common') {
-                    dependency = createNodeDependence(dependancy, '5.1.0', NodeDependencyType.Default);
-                } else if (dependancy === '@paddls/ngx-serializer') {
-                    dependency = createNodeDependence(dependancy, '2.0.1', NodeDependencyType.Default);
-                }
-            } else if (angularVersion === 13 && dependancy !=='@paddls/rxjs-common' && dependancy !== '@paddls/ts-serializer') {
-                if (dependancy === '@paddls/ngx-repository' || dependancy === '@paddls/ngx-http-repository' || dependancy === '@paddls/ngx-firestore-repository') {
-                    dependency = createNodeDependence(dependancy, '3.1.0', NodeDependencyType.Default);
-                } else if (dependancy === '@paddls/ngx-common') {
-                    dependency = createNodeDependence(dependancy, '4.0.0', NodeDependencyType.Default);
-                } else if (dependancy === '@paddls/ngx-serializer') {
-                    dependency = createNodeDependence(dependancy, '1.0.9', NodeDependencyType.Default);
-                }
-            } else if (angularVersion === 12 && dependancy != '@paddls/rxjs-common' && dependancy != '@paddls/ts-serializer') {
-                if (dependancy === '@paddls/ngx-repository' || dependancy === '@paddls/ngx-http-repository' || dependancy === '@paddls/ngx-firestore-repository') {
-                    dependency = createNodeDependence(dependancy, '3.1.0', NodeDependencyType.Default);
-                } else if (dependancy === '@paddls/ngx-common') {
-                    dependency = createNodeDependence(dependancy, '3.0.1', NodeDependencyType.Default);
-                } else if (dependancy === '@paddls/ngx-serializer') {
-                    dependency = createNodeDependence(dependancy, '1.0.9', NodeDependencyType.Default);
-                }
-            } else if (angularVersion && angularVersion < 12 && dependancy != '@paddls/rxjs-common' && dependancy != '@paddls/ts-serializer') {
-                if (dependancy === '@paddls/ngx-repository' || dependancy === '@paddls/ngx-http-repository' || dependancy === '@paddls/ngx-firestore-repository') {
-                    dependency = createNodeDependence(dependancy, '3.1.0', NodeDependencyType.Default);
-                } else if (dependancy === '@paddls/ngx-common') {
-                    dependency = createNodeDependence(dependancy, '2.0.5', NodeDependencyType.Default);
-                } else if (dependancy === '@paddls/ngx-serializer') {
-                    dependency = createNodeDependence(dependancy, '1.0.9', NodeDependencyType.Default);
-                }
-            } else if (rxjsVersion && rxjsVersion < 7 && dependancy === '@paddls/rxjs-common') {
-                dependency = createNodeDependence(dependancy, '1.3.5', NodeDependencyType.Default);
-            }
-            else {
-                dependency = createNodeDependence(dependancy, output[output.length - 1], NodeDependencyType.Default);
-            }
-            if (dependency) {
-                addPackageJsonDependency(tree, dependency);
-            }
         });
-        context.addTask(new NodePackageInstallTask());
-        return tree;
-    };
+        let rule: Rule = () => {
+            return (tree: Tree, context: SchematicContext) => {
+                context.addTask(new NodePackageInstallTask());
+                return tree;
+            }
+        };
+        rules.push(rule)
+        return chain(
+            rules
+        );
+    }
+}
+
+function addPaddls(dep: string, version: string) {
+    let dependency: NodeDependency | undefined;
+    dependency = createNodeDependence(dep, version, NodeDependencyType.Default);
+    return dependency;
+}
+
+export function addNgxRepo(options: any): Rule {
+    return (tree:Tree) => {
+        const angularVersion: number | undefined = getAngularVersion(tree);
+        let dep: NodeDependency;
+
+        if (!angularVersion) {
+            return tree;
+        }
+
+        if (options.versionPaddls) {
+            dep = addPaddls('@paddls/ngx-repository', options.versionPaddls);
+        } else if (angularVersion === 14) {
+            dep = addPaddls('@paddls/ngx-repository', '4.0.1');
+        } else if (angularVersion === 13) {
+            dep = addPaddls('@paddls/ngx-repository', '3.1.0')
+        } else if (angularVersion <= 12) {
+            dep = addPaddls('@paddls/ngx-repository', '2.0.5')
+        } else {
+            const latest = getLatestversionPaddls('@paddls/ngx-repository');
+            dep = addPaddls('@paddls/ngx-repository', latest);
+        }
+        addPackageJsonDependency(tree,dep);
+    }
+}
+
+export function addNgxHttp(options: any): Rule {
+    return (tree:Tree) => {
+        const angularVersion: number | undefined = getAngularVersion(tree);
+        let dep: NodeDependency;
+
+        if (!angularVersion) {
+            return tree;
+        }
+
+        if (options.versionPaddls) {
+            dep = addPaddls('@paddls/ngx-http-repository', options.versionPaddls);
+        } else if (angularVersion === 14) {
+            dep = addPaddls('@paddls/ngx-http-repository', '4.0.1');
+        } else if (angularVersion === 13) {
+            dep = addPaddls('@paddls/ngx-http-repository', '3.1.0')
+        } else if (angularVersion <= 12) {
+            dep = addPaddls('@paddls/ngx-http-repository', '2.0.5')
+        } else {
+            const latest = getLatestversionPaddls('@paddls/ngx-http-repository');
+            dep = addPaddls('@paddls/ngx-http-repository', latest);
+        }
+        addPackageJsonDependency(tree,dep);
+    }
+}
+
+export function addNgxFirestore(options: any): Rule {
+    return (tree:Tree) => {
+        const angularVersion: number | undefined = getAngularVersion(tree);
+        let dep: NodeDependency;
+
+        if (!angularVersion) {
+            return tree;
+        }
+
+        if (options.versionPaddls) {
+            dep = addPaddls('@paddls/ngx-firestore-repository', options.versionPaddls);
+        } else if (angularVersion === 14) {
+            dep = addPaddls('@paddls/ngx-firestore-repository', '4.0.1');
+        } else if (angularVersion === 13) {
+            dep = addPaddls('@paddls/ngx-firestore-repository', '3.1.0')
+        } else if (angularVersion <= 12) {
+            dep = addPaddls('@paddls/ngx-firestore-repository', '2.0.5')
+        } else {
+            const latest = getLatestversionPaddls('@paddls/ngx-firestore-repository');
+            dep = addPaddls('@paddls/ngx-firestore-repository', latest);
+        }
+        addPackageJsonDependency(tree,dep);
+    }
+}
+
+export function addNgxCommon(options: any): Rule {
+    return (tree:Tree) => {
+        const angularVersion: number | undefined = getAngularVersion(tree);
+        let dep: NodeDependency;
+
+        if (!angularVersion) {
+            return tree;
+        }
+
+        if (options.versionPaddls) {
+            dep = addPaddls('@paddls/ngx-common', options.paddlsVersion);
+        } else if (angularVersion === 14) {
+            dep = addPaddls('@paddls/ngx-common', '5.1.0');
+        } else if (angularVersion <= 13) {
+            dep = addPaddls('@paddls/ngx-common', '4.0.0')
+        } else if (angularVersion <= 12) {
+            dep = addPaddls('@paddls/ngx-common', '3.0.1')
+        } else {
+            const latest = getLatestversionPaddls('@paddls/ngx-common');
+            dep = addPaddls('@paddls/ngx-common', latest);
+        }
+        addPackageJsonDependency(tree,dep);
+    }
+}
+
+export function addNgxSerializer(options: any): Rule {
+    return (tree:Tree) => {
+        const angularVersion: number | undefined = getAngularVersion(tree);
+        let dep: NodeDependency;
+
+        if (!angularVersion) {
+            return tree;
+        }
+
+        if (options.versionPaddls) {
+            dep = addPaddls('@paddls/ngx-serializer', options.paddlsVersion);
+        } else if (angularVersion === 14) {
+            dep = addPaddls('@paddls/ngx-serializer', '2.0.1');
+        } else if (angularVersion <= 13) {
+            dep = addPaddls('@paddls/ngx-serializer', '1.0.9');
+        } else {
+            const latest = getLatestversionPaddls('@paddls/ngx-serializer');
+            dep = addPaddls('@paddls/ngx-serializer', latest);
+        }
+        addPackageJsonDependency(tree,dep);
+    }
+}
+
+export function addRxjsCommon(options: any): Rule {
+    return (tree:Tree) => {
+        const rxjsVersion: number | undefined = getRxjsVersion(tree);
+        let dep: NodeDependency;
+
+        if (!rxjsVersion) {
+            return tree;
+        }
+
+        if (options.versionPaddls) {
+            dep = addPaddls('@paddls/rxjs-common', options.paddlsVersion);
+        } else if (rxjsVersion < 7) {
+            dep = addPaddls('@paddls/rxjs-common', '1.3.5');
+        } else {
+            const latest = getLatestversionPaddls('@paddls/rxjs-common');
+            dep = addPaddls('@paddls/rxjs-common', latest);
+        }
+        addPackageJsonDependency(tree,dep);
+    }
+}
+
+export function addTsSerializer(options: any): Rule {
+    return (tree:Tree) => {
+        let dep: NodeDependency;
+
+        if (options.paddlsVersion) {
+            dep = addPaddls('@paddls/ts-serializer', options.paddlsVersion);
+        } else {
+            const latest = getLatestversionPaddls('@paddls/ts-serializer');
+            dep = addPaddls('@paddls/ts-serializer', latest);
+        }
+        addPackageJsonDependency(tree,dep);
+    }
+
 }
 
 function createNodeDependence(name: string, version: string, type: NodeDependencyType): NodeDependency {
@@ -129,4 +265,15 @@ function getRxjsVersion(tree: Tree) {
         }
         return rxjsVersion;
     }
+}
+
+function getLatestversionPaddls(dep: string): string {
+    const command: string = `npm show ${dep} versions`;
+    let output: string = execSync(command, {
+        encoding: 'utf8',
+    });
+
+    output = output.replace(/'/g, '"');
+    output = JSON.parse(output);
+    return output[output.length-1];
 }
